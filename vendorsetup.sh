fcbf6f169934798e46967ddeceef4f48581e4aa3
Burhan Siddiquee <burhanverse@gmail.com>
2024-02-06 23:41:41 +0530

    lava: Add vendorsetup.sh to fetch dependencies

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# dependencies
+git clone --depth 1 https://github.com/Redmi-MT6768/android_kernel_xiaomi_mt6768 -b eleven kernel/xiaomi/mt6768
+
+git clone --depth 1 https://github.com/Redmi-MT6768/android_vendor_xiaomi_lava -b eleven vendor/xiaomi/lava
+
+git clone --depth 1 https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
+
+git clone --depth 1 https://github.com/PixelExperience/device_mediatek_sepolicy_vndr -b eleven device/mediatek/sepolicy_vndr
